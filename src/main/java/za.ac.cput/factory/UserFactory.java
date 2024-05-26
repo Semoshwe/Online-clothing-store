d634560affcc5e82db4dc657c0fde24f71ee18d4
Semoshwe <semoshwemapokgole@gmail.com>
2024-05-26 23:39:44 +0200

    userFactory test updated

--- src/main/java/za.ac.cput/factory/UserFactory.java
+++ src/main/java/za.ac.cput/factory/UserFactory.java
@@ -5,14 +5,7 @@ import za.ac.cput.domain.User;
 import za.ac.cput.util.Helper;
 
 public class UserFactory {
-    //   Address address = new
-//    public static User buildUser(String userID,String firstName, String lastName, String password, String email , Address address){
-//        if (Helper.isNullOrEmpty(userID) || Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName) || Helper.isNullOrEmpty(password)|| Helper.isEmailValid(email) || address ==null)
-//        return null;
-//
-//        return new User.Builder().setUserID(userID).setFirstName(firstName).setLastName(lastName).setPassword(password).setEmail(email).setAddress(address).build();
-//
-//    }
+
     public static User buildingUser(String userID,String firstName, String lastName, String password, String email,
                                     String addressId,String street, String city, String province, String zipCode ){
         if (Helper.isNullOrEmpty(userID)|| Helper.isNullOrEmpty(firstName) || Helper.isNullOrEmpty(lastName) ||
